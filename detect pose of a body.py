d22acaa3b2ca2d2902dc135fe8bd2cef60e772cc
Md Shakibul Alam <70951378+Alam2002@users.noreply.github.com>
2021-09-27 00:34:04 +0600

    Update detect pose of a body.py

--- detect pose of a body.py
+++ detect pose of a body.py
@@ -20,7 +20,7 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+    cv2.putText(img,"fps: "+str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
 
 
     cv2.imshow("Video",img)
