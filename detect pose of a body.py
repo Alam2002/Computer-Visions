4cef14cb56084276e612374490b1b521fbd80a16
Md Shakibul Alam <70951378+Alam2002@users.noreply.github.com>
2021-09-27 00:04:50 +0600

    Add files via upload

---
+++ detect pose of a body.py
@@ -0,0 +1,27 @@
+import cv2
+import mediapipe as mp
+import time
+
+capture = cv2.VideoCapture("run.mp4")# Here you can use any of your video in your directory
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
+pTime = 0
+
+while True:
+    success, img = capture.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img,results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+
+
+    cv2.imshow("Video",img)
+    cv2.waitKey(1)
